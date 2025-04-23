78da61feb26b95a816a0917332f8adb5bb3dea4a
Dongsusin <auroratime020715@gmail.com>
2025-04-23 09:31:47 +0900

    슬라이드 수정

--- src/App.js
+++ src/App.js
@@ -11,14 +11,13 @@ import { Footer } from "./component/footer";
 function App() {
   useEffect(() => {
     //슬라이더
-    let slides = document.querySelector(".slides"), //슬라이더 전체
-      slide = document.querySelectorAll(".slides li"), //슬라이더 1페이지
-      currentIdx = 0, //현재 페이지
-      slideCount = slide.length, //슬라이더 갯수
-      slideWidth = window.innerWidth, //슬라이더 크기
-      prevBtn = document.querySelector(".prev"), //이전 페이지 버튼
-      nextBtn = document.querySelector(".next"); //다음 페이지 버튼
-    //슬라이더 복제본 생성
+    let slides = document.querySelector(".slides"),
+      slide = document.querySelectorAll(".slides li"),
+      currentIdx = 0,
+      slideCount = slide.length,
+      slideWidth = window.innerWidth,
+      prevBtn = document.querySelector(".prev"),
+      nextBtn = document.querySelector(".next");
     function makeClone() {
       for (let i = 0; i < slideCount; i++) {
         let cloneSlide = slide[i].cloneNode(true);
@@ -36,38 +35,43 @@ function App() {
         slides.classList.add("animated");
       }, 100);
     }
-    //슬라이더 길이 계산
     function updateWidth() {
       let currentSlides = document.querySelectorAll(".slides li");
       let newSlideCount = currentSlides.length;
       let newWidth = slideWidth * newSlideCount + "px";
       slides.style.width = newWidth;
     }
-    //슬라이더 위치 초기화
     function setinit() {
       let TranslateValue = -slideWidth * slideCount;
       slides.style.transform = "translateX(" + TranslateValue + "px)";
     }
-    //다음 페이지 버튼 클릭
     nextBtn.addEventListener("click", function () {
       moveSlide(currentIdx + 1);
+      makeClone();
     });
-    //이전 페이지 버튼 클릭
     prevBtn.addEventListener("click", function () {
       moveSlide(currentIdx - 1);
+      makeClone();
     });
-    //페이지 이동
     function moveSlide(num) {
       slides.style.left = -num * slideWidth + "px";
       currentIdx = num;
       console.log(currentIdx, slideCount);
-      makeClone();
+      if (currentIdx === slideCount || currentIdx === -slideCount) {
+        setTimeout(function () {
+          slides.classList.remove("animated");
+          slides.style.left = "0px";
+          currentIdx = 0;
+        }, 500);
+        setTimeout(function () {
+          slides.classList.add("animated");
+        }, 600);
+      }
     }
-    //페이지 자동이동
-    setInterval(sliderEffect, 8000);
     function sliderEffect() {
       moveSlide(currentIdx + 1);
     }
+    setInterval(sliderEffect, 8000);
   }, []);
 
   return (
